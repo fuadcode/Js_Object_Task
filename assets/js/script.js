16f996f606911187972d3d1dd018f1f8838e9005
fuadcode <fuadgi@code.edu.az>
2024-03-07 23:59:18 +0400

    Js_Object_Task

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+
+
+//Task-1
+
+// function NumberSort(n, m) {
+//     return m - n; 
+//   }
+  
+//   let numbers = [17, 6, 1, 9, 2, 11, 5, 16];
+//   numbers.sort(NumberSort);
+//   console.log(numbers);
+  
+
+//Task-2
+
+// function SumText(stringtext) {
+  
+//     let resultWord = [];
+//     let word = stringtext.split(' ');
+
+    
+//     for (let i = 0; i < word.length; i++) {
+//        let text = word[i];
+
+        
+//         for (let n = 0; n < text.length; n++) {
+//             let a = text[n];
+
+//             resultWord.push(a);
+//         }
+//     }
+
+//     return resultWord;
+// }
+
+// let stagetext = "Code Academy";
+// let resultText = SumText(stagetext);
+
+// console.log(resultText);
+
